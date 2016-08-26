a842b7ad045668a79dd675bc1754cdc2d99d0cfe
Sam Rash <srash@fb.com>
2016-08-26 12:07:56 -0700

    log hg root on hg time log

--- hg-wrapper.pl
+++ hg-wrapper.pl
@@ -2,19 +2,19 @@
 
 use Time::HiRes qw/ time sleep /;
 use POSIX qw/strftime/;
-my $logDir;
+my $logDir = "/Users/srash/hg-time-log";
 my $hg = "/opt/facebook/bin/hg";
+my $hgRootBin = "/Users/srash/bin/hg-root.pl";
 
 if (-e "/etc/fbwhoami") {
   $hg = "hg";
-}
-
-if (-e "/etc/fbwhoami") {
   $logDir = "/home/srash/hg-time-log";
-} else {
-  $logDir = "/Users/srash/hg-time-log";
+  $hgRootBin = "/home/srash/bin/hg-root.pl";
 }
 
+
+my $hgRoot = `$hgRootBin`;
+chomp($hgRoot);
 my $date = strftime("%Y-%m-%d", localtime);
 my $datetime = strftime("%Y-%m-%dT%H:%M:%S", localtime);
 my $log = "$logDir/$date.out";
@@ -25,5 +25,5 @@ system("$hg @ARGV");
 my $ed = time();   
 my $d = $ed - $st;
 open(F, ">> $log");
-print F "$d, $datetime, $cmd\n";
+print F "$d, $datetime, $hgRoot, $cmd\n";
 close(F);
