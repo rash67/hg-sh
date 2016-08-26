f5d700faf11a3b8bbae46c84ac8821da9e8fd240
Sam Rash <srash@fb.com>
2016-08-26 11:54:18 -0700

    make dev server log hg times

--- hg-wrapper.pl
+++ hg-wrapper.pl
@@ -2,10 +2,17 @@
 
 use Time::HiRes qw/ time sleep /;
 use POSIX qw/strftime/;
+my $logDir;
+
+if (-e "/etc/fbwhoami") {
+  $logDir = "/home/srash/hg-time-log";
+} else {
+  $logDir = "/Users/srash/hg-time-log";
+}
 
 my $date = strftime("%Y-%m-%d", localtime);
 my $datetime = strftime("%Y-%m-%dT%H:%M:%S", localtime);
-my $log = "/Users/srash/hg-time-log/$date.out";
+my $log = "$logDir/$date.out";
 my $st = time();
 my $hg = "/opt/facebook/bin/hg";
 my $cmd = "$hg @ARGV";
